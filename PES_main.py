9370f65b007cf6ec86aefe027d97513924c8f605
berylramadhian <berylramadhian@gmail.com>
2021-10-29 16:12:48 +0200

    added ds and ds for tests

--- PES_main.py
+++ PES_main.py
@@ -34,29 +34,36 @@ if __name__ == "__main__":
         V = np.concatenate(V_list)
 
         
-        Fs = [pmodel.f_diatomic_vdw, pmodel.f_diatomic_chipr_ohplus]
-        F_names = ["ansatz", "CHIPR"]
+        Fs = [pmodel.f_diatomic_vdw, pmodel.f_diatomic_chipr_ohplus, pmodel.f_diatomic_dn, pmodel.f_diatomic_ds]
+        F_names = ["ansatz", "CHIPR", "Deiters-Neumaier", "Deiters-Sadus"]
+        M = 7; m = 4;
+        par = 3*M+1
+        len_Cs = [par, par, 5, 8] #number of free parameters
         Z = 8
-        M = 7; m = 4; len_C = 3*M + 1
         restarts = 10; powers = 3; delta = 1e-5
-        args = [(R,Z,M), (R,Z,M,m)]
+        args = [(R,Z,M), (R,Z,M,m), (R,), (R,)]
         rmses = []; Cs = []
         for i, f in enumerate(Fs):
+            len_C = len_Cs[i]
             rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
             rmses.append(rmse); Cs.append(C)
             print(F_names[i], "rmse = ",rmse)
-        print(rmses)
-        print(Cs)
+
         
         data = {}
         data["num_params"] = len_C #obj params
         data["opt_restart"] = restarts; data["opt_power"] = powers; data["opt_delta"] = delta #opt params
         data["mol"] = mol; #dataset descriptor
-        data["chipr_acc"] = rmses[1]; data["chipr_C"] = Cs[1] 
         data["ansatz_acc"] = rmses[0]; data["ansatz_C"] = Cs[0]
+        data["chipr_acc"] = rmses[1]; data["chipr_C"] = Cs[1]
+        data["dn_acc"] = rmses[2]; data["dn_C"] = Cs[2]
+        data["ds_acc"] = rmses[3]; data["ds_C"] = Cs[3]
         print(data)
         
-        df = pd.DataFrame.from_dict(data)
+        #df = pd.DataFrame.from_dict(data)
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df = df.transpose() #because the lengths of C are not the same
+        print(df)
         df.to_pickle("result/res_joint_"+mol+"_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl")
         
     def each_state_fit():    
@@ -74,18 +81,23 @@ if __name__ == "__main__":
         print(rel_datasets)
         
         #model training:
-        Fs = [pmodel.f_diatomic_vdw, pmodel.f_diatomic_chipr_ohplus]
-        F_names = ["ansatz", "CHIPR"]
+        #Fs = [pmodel.f_diatomic_vdw, pmodel.f_diatomic_chipr_ohplus]
+        Fs = [pmodel.f_diatomic_vdw, pmodel.f_diatomic_chipr_ohplus, pmodel.f_diatomic_dn, pmodel.f_diatomic_ds]
+        F_names = ["ansatz", "CHIPR", "Deiters-Neumaier", "Deiters-Sadus"]
+        M = 7; m = 4;
+        par = 3*M+1
+        len_Cs = [par, par, 5, 8] #number of free parameters
         Z = 8 #change Z for each molecule!!
-        M = 7; m = 4; len_C = 3*M + 1
         restarts = 10; powers = 3; delta = 1e-5
         
         data = {}
-        data["num_params"] = len_C
+        data["num_params"] = [] #only for variable free params, less relevant for fixed params e.g. f-dn or f-ds
         data["opt_restart"] = restarts; data["opt_power"] = powers; data["opt_delta"] = delta
         data["mol"] = []; data["state"] = []; data["author"] = []; data["method"] = []; data["Z"] = [] #dataset descriptor
-        data["chipr_acc"] = []; data["chipr_C"] = []
         data["ansatz_acc"] = []; data["ansatz_C"] = []
+        data["chipr_acc"] = []; data["chipr_C"] = []
+        data["dn_acc"] = []; data["dn_C"] = []
+        data["ds_acc"] = []; data["ds_C"] = []
         with warnings.catch_warnings(record=True): #CHIPR NaN problem
             for dset in rel_datasets:
                 print("dataset = ", dset["mol"], dset["state"], dset["author"])
@@ -96,16 +108,18 @@ if __name__ == "__main__":
                     data["method"].append(None)
                 Z = Zs[dset["mol"]] #assign Z
                 V = dset["V"]; R = dset["R"]
-                args = [(R,Z,M), (R,Z,M,m)]
+                args = [(R,Z,M), (R,Z,M,m), (R,), (R,)]
                 rmses = []; Cs = []
                 for i, f in enumerate(Fs):
+                    len_C = len_Cs[i]
                     rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
                     rmses.append(rmse); Cs.append(C)
                     print(F_names[i], "rmse = ",rmse)
-                    print(rmses)
-                    print(Cs)
-                data["ansatz_acc"].append(rmses[0]); data["chipr_acc"].append(rmses[1])
-                data["ansatz_C"].append(Cs[0]); data["chipr_C"].append(Cs[1])
+                    #print(rmses)
+                    #print(Cs)
+                    data["num_params"].append(len_C)
+                data["ansatz_acc"].append(rmses[0]); data["chipr_acc"].append(rmses[1]); data["dn_acc"].append(rmses[2]); data["ds_acc"].append(rmses[3])
+                data["ansatz_C"].append(Cs[0]); data["chipr_C"].append(Cs[1]); data["dn_C"].append(Cs[2]); data["ds_C"].append(Cs[3])
                 data["Z"].append(Z)
                 
         print(data)  
@@ -113,4 +127,4 @@ if __name__ == "__main__":
         df.to_pickle("result/res_each_state_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl")
         
     '''end of main functions, actual main starts below'''
-    each_state_fit()
+    joint_fit()
