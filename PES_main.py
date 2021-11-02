448311ed9781b747bc65f2fe7a6f76447f93fe8d
berylramadhian <berylramadhian@gmail.com>
2021-11-02 15:15:19 +0100

    added tests for ansatz_1 and changed saving method to pickle

--- PES_main.py
+++ PES_main.py
@@ -5,13 +5,116 @@ Created on Thu Apr  1 14:35:09 2021
 @author: Saint8312
 """
 
-import numpy as np, pandas as pd
-import nbimporter #ipython importer
+import numpy as np, pandas as pd, pickle, json
 import PES_models as pmodel, PES_data_processor as pdata
 #utilities:
 import time, datetime, warnings
 
 if __name__ == "__main__":
+    def performance_comparison():
+        '''compares the performance of each method'''
+        '''
+        performance evaluation per dataset contains:
+        "mol": molecule data name
+        "F": the functions list
+        "F_names": the name of the functions
+        "rmse": list of rmse 
+        "time": list of time evaluation,
+        "C_best": list of 1D array of best coefficients found,
+        "M": polynomial degree
+        "num_params": the number of parameters
+        
+        by varying the free parameters:
+        CHIPR: 3(m+1) + M 
+        ansatz_0: 3M+1
+        ansatz_1: 4M
+        M is common parameter, hence it is used as the reference,
+        m = 2M-2/3, where 2M-2%3 = 0 for ansatz_0; m = M-1 for ansatz_1
+        '''
+        list_data = np.load('data/hxoy_data.npy', allow_pickle=True)
+        list_data = list_data[()]
+        
+        mol = "OH+"
+        qidxes = pdata.query_one_var_indices(mol, "mol", list_data) #pick one
+        R = list_data[qidxes[1]]["R"]; V = list_data[qidxes[1]]["V"]
+        print(R, V)
+        
+        
+        Fs = [pmodel.f_diatomic_ansatz_1, pmodel.f_diatomic_chipr_ohplus]
+        F_names = ["ansatz_1","CHIPR"]
+        
+        
+        loop = int(1e4); n = 20; # # of loops per run and evaluation runs
+        restarts = int(20); powers = int(5); # number of optimization restarts and powers for random number generations
+        delta = 1e-5 #error tolerance to stop iterations
+
+        '''
+        loop = 1; n = 1; restarts = 1; powers  = 1
+        delta = 1e-5 #error tolerance to stop iterations
+        '''
+        #physical params:
+        Z = 8 #for OH or its ions
+        
+        data = {}
+        data["num_params"] = []
+        data["eval_loop"] = loop; data["eval_run"] = n;
+        data["opt_restart"] = restarts; data["opt_power"] = powers; data["opt_delta"] = delta
+        data["chipr_t"] = []; data["chipr_acc"] = []; data["chipr_C"] = []
+        #data["ansatz_0_t"] = []; data["ansatz_0_acc"] = []; data["ansatz_0_C"] = []
+        data["ansatz_1_t"] = []; data["ansatz_1_acc"] = []; data["ansatz_1_C"] = []
+        data["degree"] = []
+        
+        max_deg = 30 #maximum polynomial degree
+        init_time = time.time() #timer
+        for M in range(2, max_deg):
+            #if (2*M - 2)%3 == 0: # must satisfy this
+                #m = int((2*M - 2)/3)
+                #ansatz_par = 3*M+1
+            if M%4 == 0: #multiple of 4 only 
+                m = M - 1 #for new ansatz
+                ansatz_par = 4*M
+                #chipr_par = 3*(m+1)+M;
+                print("===========================")
+                print("M = ",M, ", m (chipr) =",m)
+                print("parameters = ",ansatz_par)
+                
+                #Time evaluation:
+                print(">>> Time evaluation:")
+                args = [(R,Z,M), (R,Z,M,m)] 
+                times_array = np.zeros(len(Fs))
+                len_C = ansatz_par #coef length, min(len) = 3M+1
+                for i in range(n):
+                    times_array += np.array(pmodel.evaluate_efficiency(Fs, loop, args, len_C))
+                mean_t = times_array/n
+                print("evaluation on",loop,"runs")
+                print("functions",F_names)
+                print("running times averaged over",n,"runs",mean_t)
+                
+                #Accuracy evaluation:
+                print(">>> Accuracy evaluation:")
+                rmses = []; Cs = []
+                for i, f in enumerate(Fs):
+                    rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
+                    rmses.append(rmse)
+                    Cs.append(C)
+                    print("rmse = ",rmse)
+                    
+                #append to data:
+                data["num_params"].append(len_C); data["degree"].append(M)
+                data["ansatz_1_t"].append(mean_t[0]); data["chipr_t"].append(mean_t[1])
+                data["ansatz_1_acc"].append(rmses[0]); data["chipr_acc"].append(rmses[1])
+                data["ansatz_1_C"].append(Cs[0]); data["chipr_C"].append(Cs[1])
+        
+        end_time = time.time() #timer
+        elapsed = end_time-init_time
+        data["simulation_time"] = elapsed
+        print("elapsed time =",elapsed,"s")
+        print(data)
+        #write to pandas, then to file:
+        filename = "result/performance_"+datetime.datetime.now().strftime('%d%m%Y')+".pkl"
+        with open(filename, 'wb') as handle:
+            pickle.dump(data, handle)
+    
     def joint_fit():
         '''joint fit for OH+'''
         list_data = np.load('data/hxoy_data.npy', allow_pickle=True)
@@ -34,13 +137,13 @@ if __name__ == "__main__":
         V = np.concatenate(V_list)
 
         
-        Fs = [pmodel.f_diatomic_vdw, pmodel.f_diatomic_chipr_ohplus, pmodel.f_diatomic_dn, pmodel.f_diatomic_ds]
+        Fs = [pmodel.f_diatomic_ansatz_0, pmodel.f_diatomic_chipr_ohplus, pmodel.f_diatomic_dn, pmodel.f_diatomic_ds]
         F_names = ["ansatz", "CHIPR", "Deiters-Neumaier", "Deiters-Sadus"]
         M = 7; m = 4;
         par = 3*M+1
         len_Cs = [par, par, 5, 8] #number of free parameters
         Z = 8
-        restarts = 10; powers = 3; delta = 1e-5
+        restarts = 1; powers = 1; delta = 1e-5
         args = [(R,Z,M), (R,Z,M,m), (R,), (R,)]
         rmses = []; Cs = []
         for i, f in enumerate(Fs):
@@ -60,10 +163,17 @@ if __name__ == "__main__":
         data["ds_acc"] = rmses[3]; data["ds_C"] = Cs[3]
         print(data)
         
+        '''
+        # pandas is less reliable for cross version!
         #df = pd.DataFrame.from_dict(data)
         df = pd.DataFrame.from_dict(data, orient='index')
         df = df.transpose() #because the lengths of C are not the same
         df.to_pickle("result/res_joint_"+mol+"_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl")
+        '''
+        filename = "result/res_joint_"+mol+"_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl"
+        with open(filename, 'wb') as handle:
+            pickle.dump(data, handle)
+
         
     def each_state_fit():    
         #separated fit for each state
@@ -121,12 +231,18 @@ if __name__ == "__main__":
                 data["ansatz_C"].append(Cs[0]); data["chipr_C"].append(Cs[1]); data["dn_C"].append(Cs[2]); data["ds_C"].append(Cs[3])
                 data["Z"].append(Z)
                 
-        print(data)  
+        print(data)
+        '''
         #df = pd.DataFrame.from_dict(data)
         df = pd.DataFrame.from_dict(data, orient='index') #if the lengths of C are not equal
         df = df.transpose() #because the lengths of C are not the same
         df.to_pickle("result/res_each_state_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl")
+        '''
+        filename = "result/res_each_state_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl"
+        with open(filename, 'wb') as handle:
+            pickle.dump(data, handle)
+        
         
     '''end of main functions, actual main starts below'''
-    #joint_fit()
-    each_state_fit()
+    performance_comparison()
+    
