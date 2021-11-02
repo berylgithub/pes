a81b4b854b72e6fc32ff750ebbce7dced20b48cb
berylramadhian <berylramadhian@gmail.com>
2021-11-02 18:07:32 +0100

    added warning curb for joint fits

--- PES_main.py
+++ PES_main.py
@@ -66,54 +66,55 @@ if __name__ == "__main__":
         
         max_deg = 30 #maximum polynomial degree
         init_time = time.time() #timer
-        for M in range(2, max_deg):
-            #if (2*M - 2)%3 == 0: # must satisfy this
-                #m = int((2*M - 2)/3)
-                #ansatz_par = 3*M+1
-            if M%4 == 0: #multiple of 4 only 
-                m = M - 1 #for new ansatz
-                ansatz_par = 4*M
-                #chipr_par = 3*(m+1)+M;
-                print("===========================")
-                print("M = ",M, ", m (chipr) =",m)
-                print("parameters = ",ansatz_par)
-                
-                #Time evaluation:
-                print(">>> Time evaluation:")
-                args = [(R,Z,M), (R,Z,M,m)] 
-                times_array = np.zeros(len(Fs))
-                len_C = ansatz_par #coef length, min(len) = 3M+1
-                for i in range(n):
-                    times_array += np.array(pmodel.evaluate_efficiency(Fs, loop, args, len_C))
-                mean_t = times_array/n
-                print("evaluation on",loop,"runs")
-                print("functions",F_names)
-                print("running times averaged over",n,"runs",mean_t)
-                
-                #Accuracy evaluation:
-                print(">>> Accuracy evaluation:")
-                rmses = []; Cs = []
-                for i, f in enumerate(Fs):
-                    rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
-                    rmses.append(rmse)
-                    Cs.append(C)
-                    print("rmse = ",rmse)
+        with warnings.catch_warnings(record=True): #required, otherwise the program will abruptly stops!
+            for M in range(2, max_deg):
+                #if (2*M - 2)%3 == 0: # must satisfy this
+                    #m = int((2*M - 2)/3)
+                    #ansatz_par = 3*M+1
+                if M%4 == 0: #multiple of 4 only 
+                    m = M - 1 #for new ansatz
+                    ansatz_par = 4*M
+                    #chipr_par = 3*(m+1)+M;
+                    print("===========================")
+                    print("M = ",M, ", m (chipr) =",m)
+                    print("parameters = ",ansatz_par)
                     
-                #append to data:
-                data["num_params"].append(len_C); data["degree"].append(M)
-                data["ansatz_1_t"].append(mean_t[0]); data["chipr_t"].append(mean_t[1])
-                data["ansatz_1_acc"].append(rmses[0]); data["chipr_acc"].append(rmses[1])
-                data["ansatz_1_C"].append(Cs[0]); data["chipr_C"].append(Cs[1])
-        
-        end_time = time.time() #timer
-        elapsed = end_time-init_time
-        data["simulation_time"] = elapsed
-        print("elapsed time =",elapsed,"s")
-        print(data)
-        #write to pandas, then to file:
-        filename = "result/performance_"+datetime.datetime.now().strftime('%d%m%Y')+".pkl"
-        with open(filename, 'wb') as handle:
-            pickle.dump(data, handle)
+                    #Time evaluation:
+                    print(">>> Time evaluation:")
+                    args = [(R,Z,M), (R,Z,M,m)] 
+                    times_array = np.zeros(len(Fs))
+                    len_C = ansatz_par #coef length, min(len) = 3M+1
+                    for i in range(n):
+                        times_array += np.array(pmodel.evaluate_efficiency(Fs, loop, args, len_C))
+                    mean_t = times_array/n
+                    print("evaluation on",loop,"runs")
+                    print("functions",F_names)
+                    print("running times averaged over",n,"runs",mean_t)
+                    
+                    #Accuracy evaluation:
+                    print(">>> Accuracy evaluation:")
+                    rmses = []; Cs = []
+                    for i, f in enumerate(Fs):
+                        rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
+                        rmses.append(rmse)
+                        Cs.append(C)
+                        print("rmse = ",rmse)
+                        
+                    #append to data:
+                    data["num_params"].append(len_C); data["degree"].append(M)
+                    data["ansatz_1_t"].append(mean_t[0]); data["chipr_t"].append(mean_t[1])
+                    data["ansatz_1_acc"].append(rmses[0]); data["chipr_acc"].append(rmses[1])
+                    data["ansatz_1_C"].append(Cs[0]); data["chipr_C"].append(Cs[1])
+            
+            end_time = time.time() #timer
+            elapsed = end_time-init_time
+            data["simulation_time"] = elapsed
+            print("elapsed time =",elapsed,"s")
+            print(data)
+            #write to pandas, then to file:
+            filename = "result/performance_"+datetime.datetime.now().strftime('%d%m%Y')+".pkl"
+            with open(filename, 'wb') as handle:
+                pickle.dump(data, handle)
     
     def joint_fit():
         '''joint fit for OH+'''
@@ -150,11 +151,12 @@ if __name__ == "__main__":
         args = [(R,Z,M), (R,Z,M,m)]
         rmses = []; Cs = []
         restarts = 10; powers = 5; delta = 1e-5
-        for i, f in enumerate(Fs):
-            len_C = len_Cs[i]
-            rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
-            rmses.append(rmse); Cs.append(C)
-            print(F_names[i], "rmse = ",rmse)
+        with warnings.catch_warnings(record=True): #CHIPR NaN problem
+            for i, f in enumerate(Fs):
+                len_C = len_Cs[i]
+                rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
+                rmses.append(rmse); Cs.append(C)
+                print(F_names[i], "rmse = ",rmse)
 
         
         data = {}
@@ -178,7 +180,64 @@ if __name__ == "__main__":
         filename = "result/res_joint_"+mol+"_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl"
         with open(filename, 'wb') as handle:
             pickle.dump(data, handle)
+        
+    def joint_fit_solo():
+        '''joint fit for OH+, solo for faster simulation time, for checking one formula/ansatz'''
+        list_data = np.load('data/hxoy_data.npy', allow_pickle=True)
+        list_data = list_data[()]
+        
+        #union the data of OH+ mol:
+        mol = "OH+"
+        qidxes = pdata.query_one_var_indices(mol, "mol", list_data)
+        #test on idx 1, train on idx 4:
+        #print(list_data[qidxs])
+        
+        R_list = []; V_list = []
+        for i in qidxes:
+            R = list_data[i]["R"]
+            V = list_data[i]["V"]
+            R_list.append(R); V_list.append(V)
+            
+        R_list = np.array(R_list); V_list = np.array(V_list)
+        R = np.concatenate(R_list)
+        V = np.concatenate(V_list)
 
+        
+        Fs = [pmodel.f_diatomic_ansatz_1]
+        F_names = ["ansatz_1", "CHIPR"]
+        M = 10
+        par = 4*M
+        len_Cs = [par]
+        Z = 8
+        #args = [(R,Z,M), (R,Z,M,m), (R,), (R,)]
+        args = [(R,Z,M)]
+        rmses = []; Cs = []
+        restarts = 10; powers = 5; delta = 1e-5
+        with warnings.catch_warnings(record=True): #CHIPR NaN problem
+            for i, f in enumerate(Fs):
+                len_C = len_Cs[i]
+                rmse, C = pmodel.multiple_multistart(restarts, powers, delta, f, V, *args[i], len_C=len_C, mode="default")
+                rmses.append(rmse); Cs.append(C)
+                print(F_names[i], "rmse = ",rmse)
+
+        
+        data = {}
+        data["num_params"] = len_C #obj params
+        data["opt_restart"] = restarts; data["opt_power"] = powers; data["opt_delta"] = delta #opt params
+        data["mol"] = mol; #dataset descriptor
+        data["ansatz_1_acc"] = rmses[0]; data["ansatz_1_C"] = Cs[0]
+        print(data)
+        
+        '''
+        # pandas is less reliable for cross version!
+        #df = pd.DataFrame.from_dict(data)
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df = df.transpose() #because the lengths of C are not the same
+        df.to_pickle("result/res_joint_"+mol+"_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl")
+        '''
+        filename = "result/res_joint_solo_"+mol+"_"+datetime.datetime.now().strftime('%d%m%y_%H%M%S')+".pkl"
+        with open(filename, 'wb') as handle:
+            pickle.dump(data, handle)
         
     def each_state_fit():    
         #separated fit for each state
@@ -249,6 +308,6 @@ if __name__ == "__main__":
         
         
     '''end of main functions, actual main starts below'''
-    #performance_comparison()
-    joint_fit()
+    performance_comparison()
+    #joint_fit()
     
