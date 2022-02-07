6bd8994e35cfd5eac8ada2e1e6f16190dc4b4177
berylramadhian <berylramadhian@gmail.com>
2022-02-07 23:09:49 +0100

    h2 abinitio test attempt

--- PES_abinitio.py
+++ PES_abinitio.py
@@ -2,27 +2,31 @@ import numpy as np
 import psi4
 import time
 
-ne2_geometry = """
-Ne
+h2_geometry = """
+H
 --
-Ne 1 {0}
+H 1 {0}
 """
 
-Rvals = [2.5, 3.0, 4.0, 4.5, 4.6]
+Rvals = [2.5, 3.0]
 
 psi4.set_options({'freeze_core': 'true'})
 
 # Initialize a blank dictionary of counterpoise corrected energies
 # (Need this for the syntax below to work)
 
-ecp = {}
-
+data = np.zeros((len(Rvals), 2))
 start = time.time()
 
-for R in Rvals:
-    ne2 = psi4.geometry(ne2_geometry.format(R))
-    ecp[R] = psi4.energy('ccsd(t)/aug-cc-pvdz', bsse_type='cp', molecule=ne2)
+for i, R in enumerate(Rvals):
+    mol = psi4.geometry(h2_geometry.format(R))
+    data[i][0] = R
+    data[i][1] = psi4.energy('ccsd(t)/aug-cc-pvdz', bsse_type='cp', molecule=mol)
 
+print(data)
+np.save("data/abinitio_test", data)
+
+'''
 # Prints to screen
 print("CP-corrected CCSD(T)/aug-cc-pVDZ Interaction Energies\n\n")
 print("          R [Ang]                 E_int [kcal/mol]       ")
@@ -39,6 +43,8 @@ for R in Rvals:
     e = ecp[R] * psi4.constants.hartree2kcalmol
     psi4.core.print_out("            {:3.1f}                        {:1.6f}\n".format(R, e))
 
+
+'''
 end  = time.time()
 
 print("runtime = ",end-start)
