16d95966dd93c4018026d3a4cb962bfc345740ba
Beryl Aribowo <berylramadhian@gmail.com>
2022-07-27 11:33:25 +0200

    added function to generate Z3 and D3 subsets

---
+++ completeness_check.jl
@@ -0,0 +1,36 @@
+"""
+params:
+    - m size of subsets
+    - n size of of molecule (num atoms)
+    - mode, string = "Z3", "D3"
+"""
+function generate_subsets(m, n, mode="Z3")
+    idx = 1
+    Γ = Array{Int64}(undef, 3, n, m)
+    while idx ≤ m
+        T = rand(-2:2, (3,n))
+        accept = 0
+        for i=1:n
+            if mode == "Z3" 
+                accept += check_Z3(T[:,i])
+            elseif mode == "D3"
+                accept += check_D3(T[:,i])
+            end
+        end
+        if accept == n
+            Γ[:, :, idx] = T
+            idx += 1
+        end
+    end
+    
+    return Γ
+end
+
+function check_Z3(x)
+    return sum(x .^ 2) ≤ 4
+end
+
+function check_D3(x)
+    return sum(x .^ 2) ≤ 8
+end
+
