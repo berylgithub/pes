83ae6c8d447c482f81a38c0e6322d2e2160aae40
Beryl Aribowo <berylramadhian@gmail.com>
2022-07-27 11:45:35 +0200

    completed subset generation and save to file funcion

--- completeness_check.jl
+++ completeness_check.jl
@@ -1,10 +1,21 @@
+using JLD
+
 """
 params:
     - m size of subsets
     - n size of of molecule (num atoms)
+    - file, string, directory
     - mode, string = "Z3", "D3"
 """
-function generate_subsets(m, n, mode="Z3")
+function check_Z3(x)
+    return sum(x .^ 2) ≤ 4
+end
+
+function check_D3(x)
+    return sum(x .^ 2) ≤ 8
+end
+
+function generate_subsets(m, n, file, mode="Z3")
     idx = 1
     Γ = Array{Int64}(undef, 3, n, m)
     while idx ≤ m
@@ -22,15 +33,9 @@ function generate_subsets(m, n, mode="Z3")
             idx += 1
         end
     end
-    
+    JLD.save(file, "data", Γ)
     return Γ
 end
 
-function check_Z3(x)
-    return sum(x .^ 2) ≤ 4
-end
 
-function check_D3(x)
-    return sum(x .^ 2) ≤ 8
-end
 
